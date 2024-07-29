5b15f95eb6d8200258bad2b8a4143ac35a7ed820
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-29 19:44:37 -0300

    chore: adicionado a lib viper para carregar info do .env

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config é para armazena as configurações do rate limiter.
+type Config struct {
+	LimitBy              string        `mapstructure:"LIMIT_BY"`
+	MaxRequestsPerSecond int           `mapstructure:"MAX_REQUESTS_PER_SECOND"`
+	BlockDuration        time.Duration `mapstructure:"BLOCK_DURATION_SECONDS"`
+	RedisAddr            string        `mapstructure:"REDIS_ADDR"`
+}
+
+// LoadConfig carrega as configurações do arquivo .env e variáveis de ambiente.
+func LoadConfig() (config Config, err error) {
+	// Configurar o Viper
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	// Ler o arquivo de configuração
+	err = viper.ReadInConfig()
+	if err != nil {
+		return config, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	// Mapear as configurações para a struct
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return config, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return config, nil
+}
