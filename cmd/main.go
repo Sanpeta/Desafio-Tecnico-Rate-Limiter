6122abf55dc405eec6f93e7bbc062720222542a5
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-29 20:10:39 -0300

    feat: carregando info do .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Sanpeta/rate-limiter-pos-go-expert/internal/config"
+)
+
 func main() {
+	// Carregar configurações
+	_, err := config.LoadConfig("../")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
 
 }
