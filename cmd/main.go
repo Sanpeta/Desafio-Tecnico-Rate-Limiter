78206cd50699210ca02f8577cfd41d9f89cd78e4
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-30 19:31:31 -0300

    feat: criado e iniciado o servidor.

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/Sanpeta/rate-limiter-pos-go-expert/internal/config"
+	"github.com/Sanpeta/rate-limiter-pos-go-expert/internal/limiter"
+	"github.com/Sanpeta/rate-limiter-pos-go-expert/internal/limiter/strategy"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -29,5 +32,28 @@ func main() {
 		log.Fatalf("Failed to connect to Redis: %v", err)
 	}
 	log.Println("Connected to Redis successfully!")
+
+	// Criar IPLimiter com a estratégia Redis
+	ipLimiter := limiter.NewIPLimiter(config, strategy.NewRedisStrategy(redisClient))
+
+	// Criar TokenLimiter com a estratégia Redis
+	tokenLimiter := limiter.NewTokenLimiter(config, strategy.NewRedisStrategy(redisClient))
+
+	// Escolher o limitador a ser usado
+	var limiterToUse limiter.Limiter
+	if config.LimitBy == "ip" {
+		limiterToUse = ipLimiter
+	} else {
+		limiterToUse = tokenLimiter
+	}
+
+	// Criar o roteador e adicionar o middleware
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	// Iniciar o servidor
+	log.Fatal(http.ListenAndServe(":8080", handlerWithMiddleware))
 
 }
