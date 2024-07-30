95ed7fc4712d0e70a111b497eb46d0168d86b272
Sidnei de Souza Junior <sidneidesouzajunior@Sidneis-MacBook-Air.local>
2024-07-30 14:34:23 -0300

    feat: verificando se foi estabelecido uma conexao com o Redis.

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"github.com/Sanpeta/rate-limiter-pos-go-expert/internal/config"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
 	// Carregar configurações
-	_, err := config.LoadConfig(".")
+	config, err := config.LoadConfig(".")
 	if err != nil {
-		log.Fatalf("Error loading configuration: %v", err)
+		log.Fatalf("Error loading configuration from .: %v", err)
 	}
+
+	fmt.Print(config)
+
+	// Criar cliente Redis
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: config.RedisAddr,
+	})
+
+	// Testar a conexão com o Redis
+	err = redisClient.Ping(context.Background()).Err()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+	log.Println("Connected to Redis successfully!")
 
 }
